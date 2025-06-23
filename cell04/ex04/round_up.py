d2c6c8b9dbcdec991e75c593ae5bc80473d64285
thet san <thetsanhtar0201@gmail.com>
2025-06-23 19:56:13 +0700

    .

---
+++ cell04/ex04/round_up.py
@@ -0,0 +1,3 @@
+import math
+number = float(input("Enter a rumber: "))
+print(math.ceil(number))
---
+++ cell04/ex05/up_low.py
@@ -0,0 +1,9 @@
+string = input("Enter a word: ")
+result = ""
+for char in string:
+    if char.isupper():
+        result += char.lower()
+    elif char.islower():
+        result += char.upper()
+        result += char
+print(result)
