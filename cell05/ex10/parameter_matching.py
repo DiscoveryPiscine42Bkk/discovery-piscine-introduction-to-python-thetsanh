4628da470423b7851b7274a14eca2f4a2a30121f
thet san <thetsanhtar0201@gmail.com>
2025-06-23 22:06:08 +0700

    cell 05

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,10 @@
+import sys
+n = len(sys.argv)
+if n != 2:
+    print("none")
+else:
+    inp = input("What was the parameter? ")
+    if inp == sys.argv[1]:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
