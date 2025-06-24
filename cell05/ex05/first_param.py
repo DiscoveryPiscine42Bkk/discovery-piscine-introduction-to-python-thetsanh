7a519a54a7311e9eb7a05a27ebeb82c85276ee7f
thet san <thetsanhtar0201@gmail.com>
2025-06-24 12:38:54 +0700

    cell05

---
+++ cell05/ex05/first_param.py
@@ -0,0 +1,6 @@
+import sys
+n = len(sys.argv)
+if n <= 1:
+    print("none")
+else:
+    print(sys.argv[1])
---
+++ cell05/ex08/aff_rev_parameters.py
@@ -0,0 +1,7 @@
+import sys
+n = len(sys.argv)
+if n <= 3:
+    print("none")
+else:
+    for i in range(n - 1, 0 , -1):
+        print(sys.argv[i])
