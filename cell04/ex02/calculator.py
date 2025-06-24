bca17b6bb8c2fe3be5578ed9d85d15c5c55d5f86
thet san <thetsanhtar0201@gmail.com>
2025-06-24 19:04:05 +0700

    update

--- cell04/ex02/calculator.py
+++ cell04/ex02/calculator.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
-num1 = int(input("Give me the first numbere: "))
-num2 = int(input("Give me the second numbere: "))
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
 print("Thank you!")
-for i in ['+', '-', '/', '*']:
-    if i == '+':
-        print(f"{num1} + {num2} = {num1 + num2}")
-    elif i == '-':
-        print(f"{num1} - {num2} = {num1 - num2}")
-    elif i == '/':
-        if num2 != 0:
-            print(f"{num1} / {num2} = {int(num1 / num2)}")
-        else:
-            print("Division by zero is not allowed.")
-    elif i == '*':
-        print(f"{num1} * {num2} = {num1 * num2}")
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
